3154e9379ec6435a60a14726e24b8ae9c74b40d3
Pranavg11122008 <77529021+Pranavg11122008@users.noreply.github.com>
2021-11-06 13:44:26 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 200
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke("white")
+            fill("White");
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+            pop()
+
+    }
+    
+}
---
+++ sketch.js
@@ -0,0 +1,80 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1;
+
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,100,70,70);
+    box2 = new Box(700,100,70,70);
+    box12 = new Box(700,100,70,70);
+    box13 = new Box(700,100,70,70);
+    box14 = new Box(700,100,70,70);
+
+    box15 = new Box(800,100,70,70);
+    box3 = new Box(800,100,70,70);
+    box4 = new Box(800,100,70,70);
+    box9 = new Box(800,100,70,70);
+    box8 = new Box(800,100,70,70);
+
+    box10 = new Box(900,100,70,70);
+    box11 = new Box(900,100,70,70);
+    box5 = new Box(900,100,70,70);
+    box6 = new Box(900,100,70,70);
+    box7 = new Box(900,100,70,70);
+
+    ball = new ball(100,10,50)
+    Slingshot1 = new SlingShot(ball.body,{
+        x:500,y:10
+    })
+    
+    
+}
+
+function draw(){
+    background(0);
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    ground.display();
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+    box13.display();
+    box14.display();
+    box15.display();
+    
+    ball.display();
+    Slingshot1.display();
+     
+}
+
+ function mouseDragged(){
+     Matter.Body.setPosition(ball.body,{
+         x:mouseX,y:mouseY
+     })
+ }
+
+
+
+
